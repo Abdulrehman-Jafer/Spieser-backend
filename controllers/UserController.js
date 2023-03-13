a9d28185ae130e071f0a3b9826f74130d63aceff
Abdulrehman-Jafer <abdulrehmanjaferwork01233@gmail.com>
2023-03-13 19:14:47 +0500

    used Bcrypt

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,6 @@
 import Post from "../models/POST.js";
 import User from "../models/USER.js";
+import {hashSync,compareSync} from "bcrypt"
 
 export const createNewUser = async (req, res) => {
   const { username, password, userimage } = req.body;
@@ -15,9 +16,10 @@ export const createNewUser = async (req, res) => {
   if (existingUser) {
     return res.status(400).json({ message: "Username already exist" });
   }
+  let hashedPassword = hashSync(password)
   const user = new User({
     username: username,
-    password: password,
+    password: hashedPassword,
     userimage: userimage,
   });
   try {
@@ -39,8 +41,9 @@ export const LogIn = async (req, res) => {
   if (!existingUser) {
     return res.status(404).json({ message: "User does not exist" });
   }
-  if (existingUser.password !== password) {
-    return res.status(403).json({ message: "wrong password Forbidden" });
+  let isCorrectPassword = compareSync(password,existingUser.password)
+  if (!isCorrectPassword) {
+    return res.status(403).json({ message: "Wrong password --Forbidden" });
   }
   return res.status(200).json({ existingUser });
 };
@@ -68,7 +71,7 @@ export const getUserPosts = async (req, res) => {
 export const SearchUser = async (req, res) => {
   const { searchText } = req.body;
   if (!searchText) {
-    return res.status(400).json({ message: "bad request" });
+    return res.status(400).json({ message:"bad request"});
   }
   try {
     const result = await User.find({username:searchText});
